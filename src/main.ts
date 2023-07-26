0cc49c79ea16a8cf8a2ef59184dfe0033f9f3546
alejox <alejoxdrums@gmail.com>
2023-07-26 10:37:08 -0500

    first commit

---
+++ src/main.ts
@@ -0,0 +1,37 @@
+import './assets/main.css'
+
+import { createApp } from 'vue'
+import { createPinia } from 'pinia'
+import piniaPluginPersistedstate from 'pinia-plugin-persistedstate'
+
+import axios from '@/plugins/Axios'
+import App from './App.vue'
+import router from './router'
+
+//PLUGINS
+import vuetify from './plugins/Vuetify'
+import globalComponents from './plugins/GlobalComponents'
+import './plugins/Luxon'
+
+const pinia = createPinia()
+pinia.use(piniaPluginPersistedstate)
+
+const app = createApp(App)
+
+app.config.globalProperties.mode = 'prod'
+
+//PLUGINS
+app.use(globalComponents)
+app.use(vuetify)
+
+//PROJECT BASE
+app.use(pinia)
+app.use(router)
+
+app.mount('#app')
+
+const prodApi = 'https://space-api.kodexpay.com/app/v1'
+const devApi = 'http://127.0.0.1:3332/app/v1'
+
+app.config.globalProperties.apiUrl = app.config.globalProperties.mode == 'dev' ? devApi : prodApi
+axios.defaults.baseURL = app.config.globalProperties.apiUrl
